4a43f13f3455e4f3df0abcb3e8beb5cd58559dbd
Lukasz Kuzio <lkuzio@gmail.com>
2017-02-15 20:37:48 +0100

    initial commit

--- src/main/java/xyz/javista/Application.java
+++ src/main/java/xyz/javista/Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Luke on 2017-01-20.
  */
+@Controller
 @SpringBootApplication
 public class Application {
 
@@ -17,11 +21,17 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer(){
         return (container -> {
             container.setPort(8092);
-            container.setContextPath(CONTEXT);
+            //container.setContextPath(CONTEXT);
         });
     }
 
